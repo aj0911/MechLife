60a355c05ac4965929b73711ff1c3ed22ade7f8b
abhinav jha <jhaabhinav16@gmail.com>
2023-07-08 15:09:56 +0530

    modified about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,6 +5,10 @@ import {FiPhoneCall} from 'react-icons/fi'
 import {BiSolidWallet,BiCurrentLocation} from 'react-icons/bi'
 import {MdOutlineVerifiedUser} from 'react-icons/md'
 import {FaTools,FaGasPump} from 'react-icons/fa'
+import Process1 from '../../Assets/process-1.jpg'
+import Process2 from '../../Assets/process-2.jpg'
+import Process3 from '../../Assets/process-3.jpg'
+import Process4 from '../../Assets/process-4.jpg'
 
 const About = () => {
   const cards = [
@@ -39,6 +43,25 @@ const About = () => {
       text:'Find our top quality services we offer to our dear client over time.'
     },
   ]
+  const process = [
+    {
+      img:Process1,
+      text:'choose your service'
+    },
+    {
+      img:Process2,
+      text:'make an appointment'
+    },
+    {
+      img:Process3,
+      text:'confirm your request'
+    },
+    {
+      img:Process4,
+      text:'pick your car'
+    },
+  ]
+
   return (
     <div id='About' className="about">
         <Title num={'01'} title={'About Our Company'}/>
@@ -54,6 +77,27 @@ const About = () => {
                   <div className="left">{e.icon()}</div>
                   <div className="right">
                     <h4>{e.title}</h4>
+                    <p>{e.text}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="workProcess">
+          <div className="intro">
+              <h3>How it works?</h3>
+              <p>Work Process for taking full benifit of India's best car repairing online portal.</p>
+          </div>
+          <div className="process">
+            {
+              process.map((e,i)=>(
+                <div className="eachProcess" key={i}>
+                  <div className="top">
+                    <img src={e.img} alt="" />
+                  </div>
+                  <div className="bottom">
+                    <h4>{i+1}</h4>
                     <p>{e.text}</p>
                   </div>
                 </div>
